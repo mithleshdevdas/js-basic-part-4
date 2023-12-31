55945dfae355f18b1f4abe346d2e70dea8012258
mithlesh devdas <mithleshdevdas580@gmail.com>
2023-12-31 18:33:08 +0530

    for each arr

---
+++ forEachloop.js
@@ -0,0 +1,29 @@
+const codinlag = ['js', 'py', 'c++', 'java']
+
+codinlag.forEach((e) => {
+    // console.log(e);
+})
+codinlag.forEach((elemet, index, arr) => {
+    // console.log(`elemet is ${elemet} index number is ${index} arr is ${arr}`);
+})
+
+const myarr = [
+    {
+        lag: 'js',
+        lagname: 'javascript'
+    },
+    {
+        lag: 'py',
+        lagname: 'python'
+    },
+    {
+        lag: 'jv',
+        lagname: 'java'
+    },
+    
+]
+myarr.forEach((item) => {
+    console.log(item.lagname);
+
+
+})
