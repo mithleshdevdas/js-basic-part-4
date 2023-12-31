281b7499f712e3e30f097d59307506d6e043f6a1
mithlesh devdas <mithleshdevdas580@gmail.com>
2023-12-31 18:33:20 +0530

    for in object

---
+++ forInloop.js
@@ -0,0 +1,24 @@
+const myobj = {
+    gaem1: 'wow',
+    game2: 'cool',
+    game3: 'amaz'
+}
+
+for (const key in myobj) {
+   
+       console.log(key ,myobj[key]); 
+}
+    
+const coding = ['js', 'py', 'c++']
+for (const key in coding) {
+    console.log(coding[key]);
+}
+
+const maps = new Map();
+map.set("in", "india");
+map.set("ca", "canada");
+map.set("ln", "london");
+
+for (const key in maps) {
+    console.log(maps[key]);
+}
